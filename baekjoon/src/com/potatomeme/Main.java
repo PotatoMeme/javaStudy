4333c59fb843ab75301e8aab44c28a247785c141
PotatoMeme <kshk0897@gmail.com>
2022-04-08 22:37:59 +0900

    20220408 백준자바단계별 조건문

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -7,13 +7,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int A, B;
-        A = in.nextInt();
-        B = in.nextInt();
-        System.out.println(A * (B % 10));
-        System.out.println(A * (B % 100 / 10));
-        System.out.println(A * (B / 100));
-        System.out.println(A * B);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int C = in.nextInt();
+        int result = 0;
+        if (A == B & A == C) {
+            result = A * 1000 + 10000;
+        } else if (A == B || A == C) {
+            result = A * 100 + 1000;
+        } else if (B == C) {
+            result = B * 100 + 1000;
+        } else {
+            result = (A > B && A > C ? A : B > A && B > C ? B : C )* 100;
+        }
+        System.out.println(result);
     }
 }
 
