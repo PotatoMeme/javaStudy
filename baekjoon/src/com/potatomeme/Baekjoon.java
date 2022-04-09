d55be402bef07ced19fd56be6d8ce961bc404873
PotatoMeme <kshk0897@gmail.com>
2022-04-10 01:47:48 +0900

    20220410 백준자바단계별 1차원 배열

--- baekjoon/src/com/potatomeme/Baekjoon.java
+++ baekjoon/src/com/potatomeme/Baekjoon.java
@@ -1429,4 +1429,421 @@ public class Baekjoon {
         System.out.println(cnt);
     }
 
+    //  4 단계 : 1차원 배열 ----------------------------------------------------------
+    //10818
+    {/*문제
+    N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+    출력
+    첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+    예제 입력 1
+    5
+    20 10 35 30 7
+    예제 출력 1
+    7 35
+    */
+    }
+
+    public void solution10818_ver1() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        int max, min;
+        max = num[0];
+        min = num[0];
+        for (int i : num) {
+            max = max > i ? max : i;
+            min = min < i ? min : i;
+        }
+        bw.write(min + " " + max);
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public void solution10818_ver2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        bw.write(Arrays.stream(num).min().getAsInt() + " " + Arrays.stream(num).max().getAsInt());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //2562
+    {/*문제
+    9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+
+    예를 들어, 서로 다른 9개의 자연수
+
+    3, 29, 38, 12, 57, 74, 40, 85, 61
+
+    이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+
+    입력
+    첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+
+    출력
+    첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+
+    예제 입력 1
+    3
+    29
+    38
+    12
+    57
+    74
+    40
+    85
+    61
+    예제 출력 1
+    85
+    8
+    */
+    }
+
+    public void solution2562_ver1() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] num = new int[9];
+        for (int i = 0; i < 9; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
+        int max, index;
+        max = num[0];
+        index = 1;
+        for (int i = 0; i < 9; i++) {
+            max = max > num[i] ? max : num[i];
+            index = max == num[i] ? i + 1 : index;
+        }
+        bw.write(max + "\n" + index);
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //2562
+    {/*문제
+    세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
+
+    예를 들어 A = 150, B = 266, C = 427 이라면 A × B × C = 150 × 266 × 427 = 17037300 이 되고, 계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
+
+    입력
+    첫째 줄에 A, 둘째 줄에 B, 셋째 줄에 C가 주어진다. A, B, C는 모두 100보다 크거나 같고, 1,000보다 작은 자연수이다.
+
+    출력
+    첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다.
+
+    예제 입력 1
+    150
+    266
+    427
+    예제 출력 1
+    3
+    1
+    0
+    2
+    0
+    0
+    0
+    2
+    0
+    0
+    */
+    }
+
+    public void solution2577() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int result = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        int[] num = new int[10];
+        while (result > 0) {
+            num[result % 10]++;
+            result = result / 10;
+        }
+        for (int i : num) {
+            bw.write(i + '\n');
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //3052
+    {/*문제
+    두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다.
+
+    수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이 아닌 정수이다.
+
+    출력
+    첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+
+    예제 입력 1
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+    예제 출력 1
+    10
+    각 수를 42로 나눈 나머지는 1, 2, 3, 4, 5, 6, 7, 8, 9, 10이다.
+
+    예제 입력 2
+    42
+    84
+    252
+    420
+    840
+    126
+    42
+    84
+    420
+    126
+    예제 출력 2
+    1
+    모든 수를 42로 나눈 나머지는 0이다.
+
+    예제 입력 3
+    39
+    40
+    41
+    42
+    43
+    44
+    82
+    83
+    84
+    85
+    예제 출력 3
+    6
+    */
+    }
+
+    public void solution3052() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] num = new int[47];
+        for (int i = 0; i < 10; i++) {
+            num[Integer.parseInt(br.readLine()) % 42]++;
+        }
+        int cnt = 0;
+        for (int i : num) {
+            if (i > 0) {
+                cnt++;
+            }
+        }
+        bw.write(String.valueOf(cnt));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //1546
+    {/*문제
+    세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+
+    예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+
+    세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 둘째 줄에 세준이의 현재 성적이 주어진다. 이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
+
+    출력
+    첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
+
+    예제 입력 1
+    3
+    40 80 60
+    예제 출력 1
+    75.0
+    예제 입력 2
+    3
+    10 20 30
+    예제 출력 2
+    66.666667
+    10-2 이하의 오차를 허용한다는 말은 정확히 소수 2번째 자리까지 출력하라는 뜻이 아니다.
+
+    예제 입력 3
+    4
+    1 100 100 100
+    예제 출력 3
+    75.25
+    예제 입력 4
+    5
+    1 2 4 8 16
+    예제 출력 4
+    38.75
+    예제 입력 5
+    2
+    3 10
+    예제 출력 5
+    65.0
+    예제 입력 6
+    4
+    10 20 0 100
+    예제 출력 6
+    32.5
+    예제 입력 7
+    1
+    50
+    예제 출력 7
+    100.0
+    예제 입력 8
+    9
+    10 20 30 40 50 60 70 80 90
+    예제 출력 8
+    55.55555555555556
+    */
+    }
+
+    public void solution1546() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+        float sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            sum += (num[i] = Integer.parseInt(st.nextToken()));
+        }
+        bw.write(String.valueOf(sum / Arrays.stream(num).max().getAsInt() * 100 / n));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //8958
+    {/*문제
+    "OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+
+    "OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+
+    OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이 주어진다. 문자열은 O와 X만으로 이루어져 있다.
+
+    출력
+    각 테스트 케이스마다 점수를 출력한다.
+
+    예제 입력 1
+    5
+    OOXXOXXOOO
+    OOXXOOXXOO
+    OXOXOXOXOXOXOX
+    OOOOOOOOOO
+    OOOOXOOOOXOOOOX
+    예제 출력 1
+    10
+    9
+    7
+    55
+    30
+    */
+    }
+
+    public void solution8958() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String[] txt = new String[n];
+        int cnt, sum;
+        for (int i = 0; i < n; i++) {
+            sum = cnt = 0;
+
+            txt[i] = br.readLine();
+            for (int j = 0; j < txt[i].length(); j++) {
+                sum += txt[i].charAt(j) == 'O' ? ++cnt : (cnt = 0);
+            }
+            bw.write(sum + " \n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //4344
+    {/*문제
+    대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+
+    입력
+    첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+
+    둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+    출력
+    각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+
+    예제 입력 1
+    5
+    5 50 50 70 80 100
+    7 100 95 90 80 70 60 50
+    3 70 90 80
+    3 70 90 81
+    9 100 99 98 97 96 95 94 93 91
+    예제 출력 1
+    40.000%
+    57.143%
+    33.333%
+    66.667%
+    55.556%
+    */
+    }
+
+    public void solution4344() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int cnt;
+        int[] list;
+        double result;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            list = new int[Integer.parseInt(st.nextToken())];
+            cnt=0;
+            for (int j = 0; j < list.length; j++) {
+                list[j] = Integer.parseInt(st.nextToken());
+            }
+            result = Arrays.stream(list).average().getAsDouble();
+            for(int j:list){
+                cnt += j>result?1:0;
+            }
+            result = (double) cnt/list.length*100;
+            bw.write(String.format("%.3f",result)+"% \n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
 }
--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -1,27 +1,34 @@
 package com.potatomeme;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int A = Integer.parseInt(br.readLine());
-        int x, y, saveData, cnt;
-        saveData = A;
-        cnt = 0;
-        do {
-            x = saveData / 10;
-            y = saveData % 10;
-            saveData = y * 10 + (x + y)%10;
-            cnt++;
-        } while (saveData != A);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int cnt;
+        int[] list;
+        double result;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            list = new int[Integer.parseInt(st.nextToken())];
+            cnt=0;
+            for (int j = 0; j < list.length; j++) {
+                list[j] = Integer.parseInt(st.nextToken());
+            }
+            result = Arrays.stream(list).average().getAsDouble();
+            for(int j:list){
+                cnt += j>result?1:0;
+            }
+            result = (double) cnt/list.length*100;
+            bw.write(String.format("%.3f",result)+"% \n");
+        }
         br.close();
-        System.out.println(cnt);
+        bw.flush();
+        bw.close();
     }
 }
 
