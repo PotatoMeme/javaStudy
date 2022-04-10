60df66faff92178e3d1d6924129a02fb8adea5cf
PotatoMeme <kshk0897@gmail.com>
2022-04-10 12:39:37 +0900

    20220410 백준자바단계별 함수

--- baekjoon/src/com/potatomeme/Baekjoon.java
+++ baekjoon/src/com/potatomeme/Baekjoon.java
@@ -1830,20 +1830,198 @@ public class Baekjoon {
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             list = new int[Integer.parseInt(st.nextToken())];
-            cnt=0;
+            cnt = 0;
             for (int j = 0; j < list.length; j++) {
                 list[j] = Integer.parseInt(st.nextToken());
             }
             result = Arrays.stream(list).average().getAsDouble();
-            for(int j:list){
-                cnt += j>result?1:0;
+            for (int j : list) {
+                cnt += j > result ? 1 : 0;
             }
-            result = (double) cnt/list.length*100;
-            bw.write(String.format("%.3f",result)+"% \n");
+            result = (double) cnt / list.length * 100;
+            bw.write(String.format("%.3f", result) + "% \n");
         }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //  5 단계 : 함수 ----------------------------------------------------------
+    //15596
+    {/*문제
+    정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
+
+    작성해야 하는 함수는 다음과 같다.
+
+    C, C11, C (Clang), C11 (Clang): long long sum(int *a, int n);
+    a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+    n: 합을 구해야 하는 정수의 개수
+    리턴값: a에 포함되어 있는 정수 n개의 합
+    C++, C++11, C++14, C++17, C++ (Clang), C++11 (Clang), C++14 (Clang), C++17 (Clang): long long sum(std::vector<int> &a);
+    a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+    리턴값: a에 포함되어 있는 정수 n개의 합
+    Python 2, Python 3, PyPy, PyPy3: def solve(a: list) -> int
+    a: 합을 구해야 하는 정수 n개가 저장되어 있는 리스트 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+    리턴값: a에 포함되어 있는 정수 n개의 합 (정수)
+    Java: long sum(int[] a); (클래스 이름: Test)
+    a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+    리턴값: a에 포함되어 있는 정수 n개의 합
+    Go: sum(a []int) int
+    a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+    리턴값: a에 포함되어 있는 정수 n개의 합
+    */
+    }
+
+    public long solution15596(int[] a) throws IOException {
+        long sum = 0;
+        for (long i : a) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    //4673
+    {/*문제
+    셀프 넘버는 1949년 인도 수학자 D.R. Kaprekar가 이름 붙였다. 양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수라고 정의하자. 예를 들어, d(75) = 75+7+5 = 87이다.
+
+    양의 정수 n이 주어졌을 때, 이 수를 시작해서 n, d(n), d(d(n)), d(d(d(n))), ...과 같은 무한 수열을 만들 수 있다.
+
+    예를 들어, 33으로 시작한다면 다음 수는 33 + 3 + 3 = 39이고, 그 다음 수는 39 + 3 + 9 = 51, 다음 수는 51 + 5 + 1 = 57이다. 이런식으로 다음과 같은 수열을 만들 수 있다.
+
+    33, 39, 51, 57, 69, 84, 96, 111, 114, 120, 123, 129, 141, ...
+
+    n을 d(n)의 생성자라고 한다. 위의 수열에서 33은 39의 생성자이고, 39는 51의 생성자, 51은 57의 생성자이다. 생성자가 한 개보다 많은 경우도 있다. 예를 들어, 101은 생성자가 2개(91과 100) 있다.
+
+    생성자가 없는 숫자를 셀프 넘버라고 한다. 100보다 작은 셀프 넘버는 총 13개가 있다. 1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97
+
+    10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+    입력
+    입력은 없다.
+
+    출력
+    10,000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 증가하는 순서로 출력한다.
+
+    예제 입력 1
+    예제 출력 1
+    1
+    3
+    5
+    7
+    9
+    20
+    31
+    42
+    53
+    64
+     |
+     |       <-- a lot more numbers
+     |
+    9903
+    9914
+    9925
+    9927
+    9938
+    9949
+    9960
+    9971
+    9982
+    9993
+    */
+    }
+
+    public void solution4673() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        boolean[] check = new boolean[10001];
+
+        for (int i = 1; i < 10001; i++) {
+            int n = solution4673_sub(i);
+            if (n < 10001) {
+                check[n] = true;
+            }
+        }
+        for (int i = 1; i < 10001; i++) {
+            if (!check[i]) {
+                bw.write(i + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+    public int solution4673_sub(int num) throws IOException {
+        int sum = num;
+        while (num != 0) {
+            sum = sum + (num % 10);
+            num = num / 10;
+        }
+        return sum;
+    }
+
+    //1065
+    {/*문제
+    어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다. 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
+
+    출력
+    첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
+
+    예제 입력 1
+    110
+    예제 출력 1
+    99
+    예제 입력 2
+    1
+    예제 출력 2
+    1
+    예제 입력 3
+    210
+    예제 출력 3
+    105
+    예제 입력 4
+    1000
+    예제 출력 4
+    144
+    예제 입력 5
+    500
+    예제 출력 5
+    119
+    */
+    }
+
+    public void solution1065() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(solution1065_sub1(Integer.parseInt(br.readLine())) + "\n");
         br.close();
         bw.flush();
         bw.close();
 
     }
+
+    public int solution1065_sub1(int num) throws IOException {
+        if (num < 100) {
+            return num;
+        } else {
+            int cnt = 99;
+            if (num == 1000) {
+                num = 999;
+            }
+
+            for (int i = 100; i <= num; i++) {
+                int hun = i / 100;
+                int ten = (i / 10) % 10;
+                int one = i % 10;
+
+                if ((hun - ten) == (ten - one)) {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+    }
+
 }
--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -7,29 +7,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int cnt;
-        int[] list;
-        double result;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            list = new int[Integer.parseInt(st.nextToken())];
-            cnt=0;
-            for (int j = 0; j < list.length; j++) {
-                list[j] = Integer.parseInt(st.nextToken());
-            }
-            result = Arrays.stream(list).average().getAsDouble();
-            for(int j:list){
-                cnt += j>result?1:0;
-            }
-            result = (double) cnt/list.length*100;
-            bw.write(String.format("%.3f",result)+"% \n");
-        }
+        bw.write(solution1065_sub1(Integer.parseInt(br.readLine()))+"\n");
         br.close();
         bw.flush();
         bw.close();
     }
+    public static int solution1065_sub1(int num) throws IOException {
+        if (num < 100) {
+            return num;
+        } else {
+            int cnt = 99;
+            if (num == 1000) {
+                num = 999;
+            }
+
+            for (int i = 100; i <= num; i++) {
+                int hun = i / 100;
+                int ten = (i / 10) % 10;
+                int one = i % 10;
+
+                if ((hun - ten) == (ten - one)) {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+    }
 }
+
 
 
