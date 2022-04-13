6b024c35ac32eb6ce1c21654796aa4a2785dff61
PotatoMeme <kshk0897@gmail.com>
2022-04-13 20:37:13 +0900

    20220413 백준자바 1712풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -7,31 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int chk = 0;
-        String str;
-        ArrayList<Character> arr;
-        Boolean test;
-        for (int i = 0; i < N; i++) {
-            str = br.readLine();
-            arr = new ArrayList<>();
-            test = true;
-            arr.add(str.charAt(0));
-            for (int j = 0; j < str.length() - 1; j++) {
-                if (str.charAt(j) != str.charAt(j + 1)) {
-                    if (arr.contains(str.charAt(j + 1))) {
-                        test = false;
-                        break;
-                    }
-                    arr.add(str.charAt(j + 1));
-                }
-            }
-            if (test) {
-                chk++;
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A, B, C;
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        int i = 0;
+        if (B >= C) {
+            i = -1;
+        } else {
+            i = A / (C - B)+1;
         }
-        bw.write(chk + "");
+        bw.write(i + "");
         br.close();
         bw.flush();
         bw.close();
