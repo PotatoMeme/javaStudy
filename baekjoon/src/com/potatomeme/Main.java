3a79c6d5d2c1a6b218a4165ac3dbe8f2071fae3b
PotatoMeme <kshk0897@gmail.com>
2022-04-14 20:30:07 +0900

    20220414 백준자바 1193풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -7,22 +7,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine()) - 1;
-        int N_save;
-        int i = 0;
-        if (N % 6 == 0) {
-            N_save = N / 6;
-        } else {
-            N_save = N / 6 + 1;
-        }
+        int N = Integer.parseInt(br.readLine());
+        int i = 1;
         while (true) {
-            N_save -= i;
-            if (N_save <= 0) {
+            N -= i;
+            if (N <= 0) {
                 break;
             }
             i++;
         }
-        bw.write(i + 1 + "");
+        if (i % 2 == 1) {
+            bw.write((1 - N) + "/" + (i + N));
+        } else {
+            bw.write((i + N) + "/" + (1 - N));
+        }
         br.close();
         bw.flush();
         bw.close();
