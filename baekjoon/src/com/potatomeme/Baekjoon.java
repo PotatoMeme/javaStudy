9cc9b6f225728e1d3730041c29ff55921e8c934d
PotatoMeme <kshk0897@gmail.com>
2022-04-15 18:09:49 +0900

    20220415 백준자바 10757풀이

--- baekjoon/src/com/potatomeme/Baekjoon.java
+++ baekjoon/src/com/potatomeme/Baekjoon.java
@@ -1,6 +1,7 @@
 package com.potatomeme;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Baekjoon {
@@ -2959,6 +2960,35 @@ public class Baekjoon {
             }
         }
         if (i < 0) bw.write(-1 + "");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    //10757
+    {/*문제
+    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+
+    출력
+    첫째 줄에 A+B를 출력한다.
+
+    예제 입력 1
+    9223372036854775807 9223372036854775808
+    예제 출력 1
+    18446744073709551615
+    */
+    }
+
+    public void solution10757() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        bw.write((A = A.add(B)).toString());
         br.close();
         bw.flush();
         bw.close();
--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -1,23 +1,17 @@
 package com.potatomeme;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int N_save, i;
-        for (i = N / 5; i >= 0; i--) {
-            N_save = N - 5 * i;
-            if (N_save % 3 == 0) {
-                bw.write(String.format("%d", i + N_save / 3));
-                break;
-            }
-        }
-        if (i < 0) bw.write(-1 + "");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        bw.write((A = A.add(B)).toString());
         br.close();
         bw.flush();
         bw.close();
