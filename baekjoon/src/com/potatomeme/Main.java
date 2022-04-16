7eafd7fc87103b2ac8fe56c365413060ca351b07
PotatoMeme <kshk0897@gmail.com>
2022-04-16 13:13:17 +0900

    20220416 백준자바 11653풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -9,33 +9,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int cnt = 0;
-        int start_num = Integer.parseInt(br.readLine());
-        int end_num = Integer.parseInt(br.readLine());
-        int sum = 0;
-        int min = 0;
-        for (int i = start_num;i<=end_num;i++) {
-            if (solution2581_sub(i)) {
-                sum += i;
-                if(sum==i) min=i;
+        int num = Integer.parseInt(br.readLine());
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            while (num % i == 0) {
+                bw.write(i+"\n");
+                num /= i;
             }
         }
-        if(sum == 0){
-            bw.write(-1+"");
-        } else{
-            bw.write(sum+"\n");
-            bw.write(min+"");
+        if (num != 1) {
+            bw.write(num+"\n");
         }
         br.close();
         bw.flush();
         bw.close();
-    }
-    public static boolean solution2581_sub(int num) {
-        if (num == 1) return false;
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
     }
 }
 
