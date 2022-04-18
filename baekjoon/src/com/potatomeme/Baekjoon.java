cc81a89a4b75244b8bea1362b063607f54002f17
PotatoMeme <kshk0897@gmail.com>
2022-04-18 18:49:23 +0900

    20220418 백준자바 1929풀이

--- baekjoon/src/com/potatomeme/Baekjoon.java
+++ baekjoon/src/com/potatomeme/Baekjoon.java
@@ -3164,4 +3164,52 @@ public class Baekjoon {
         bw.flush();
         bw.close();
     }
+
+    //1929
+    {/*문제
+    M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
+
+    출력
+    한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+
+    예제 입력 1
+    3 16
+    예제 출력 1
+    3
+    5
+    7
+    11
+    13
+    */
+    }
+    public static boolean[] prime;
+    public void solution1929() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int start_num = Integer.parseInt(st.nextToken());
+        int end_num = Integer.parseInt(st.nextToken());
+        prime = new boolean[end_num+1];
+        solution1929_sub();
+        for (int i = start_num;i<=end_num;i++) {
+            if(!prime[i]) {
+                bw.write(i+"\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    public static void solution1929_sub() {
+        prime[0] = prime[1] = true;
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
 }
--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -6,23 +6,34 @@ import java.util.*;
 import static java.lang.Math.sqrt;
 
 public class Main {
+    public static boolean[] prime;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            while (num % i == 0) {
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int start_num = Integer.parseInt(st.nextToken());
+        int end_num = Integer.parseInt(st.nextToken());
+        prime = new boolean[end_num+1];
+        solution1929_sub();
+        for (int i = start_num;i<=end_num;i++) {
+            if(!prime[i]) {
                 bw.write(i+"\n");
-                num /= i;
             }
-        }
-        if (num != 1) {
-            bw.write(num+"\n");
         }
         br.close();
         bw.flush();
         bw.close();
     }
+    public static void solution1929_sub() {
+        prime[0] = prime[1] = true;
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+
 }
 
 
