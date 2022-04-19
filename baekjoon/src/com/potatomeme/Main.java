a3998be6d7b19f929a965adb1a0c55e77cdcb670
PotatoMeme <kshk0897@gmail.com>
2022-04-19 10:00:52 +0900

    20220419 백준자바 4948풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -10,21 +10,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int start_num = Integer.parseInt(st.nextToken());
-        int end_num = Integer.parseInt(st.nextToken());
-        prime = new boolean[end_num+1];
-        solution1929_sub();
-        for (int i = start_num;i<=end_num;i++) {
-            if(!prime[i]) {
-                bw.write(i+"\n");
+        int n,cnt;
+        while (true){
+            n = Integer.parseInt(br.readLine());
+            if(n == 0){ break;}
+            prime = new boolean[2*n+1];
+            cnt = 0;
+            solution4948_sub();
+            for (int i = n+1;i<=2*n;i++) {
+                if(!prime[i]) {
+                    bw.write(i+"\n");
+                }
             }
+            bw.write(cnt+"\n");
+            bw.flush();
         }
         br.close();
         bw.flush();
         bw.close();
     }
-    public static void solution1929_sub() {
+    public static void solution4948_sub() {
         prime[0] = prime[1] = true;
         for(int i = 2; i <= Math.sqrt(prime.length); i++) {
             if(prime[i]) continue;
