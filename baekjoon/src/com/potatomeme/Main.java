7ce3a1c7f5d8b3f8e224acd5407d23b1ed5d6800
PotatoMeme <kshk0897@gmail.com>
2022-04-28 20:46:28 +0900

    20220428 백준 자바 1085 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -6,39 +6,20 @@ import java.util.*;
 import static java.lang.Math.sqrt;
 
 public class Main {
-    public static boolean[] prime;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T,n,first,second;
-        prime = new boolean[10001];
-        solution9020_sub_upgrade();
-        T = Integer.parseInt(br.readLine());
-        while(T-- > 0) {
-            n = Integer.parseInt(br.readLine());
-            first = n/2;
-            second = n/2;
-            while (true) {
-                if (!prime[first] && !prime[second]) {
-                    bw.write(first + " " + second+"\n");
-                    break;
-                }
-                first--;
-                second++;
-            }
-        }
+        int x,y,w,h;
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        int[] testCase = {x,y,w-x,h-y};
+        bw.write(Arrays.stream(testCase).min().getAsInt()+"");
         br.close();
         bw.flush();
         bw.close();
-    }
-    public static void solution9020_sub_upgrade() {
-        prime[0] = prime[1] = true;
-        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-            if(prime[i]) continue;
-            for(int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
-            }
-        }
     }
 }
 
