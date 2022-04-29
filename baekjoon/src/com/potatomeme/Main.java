f2c70fab296aa1305026409563279d30f75ebac2
PotatoMeme <kshk0897@gmail.com>
2022-04-29 21:32:34 +0900

    20220429 백준 자바 2455 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -10,21 +10,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int x,y,z;
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int x,y;
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        st.nextToken();
+        y = Integer.parseInt(st.nextToken());
+        int[] arr = new int[3];
+        arr[0] = y;
+        for(int i = 0;i<2;i++){
+            st = new StringTokenizer(br.readLine()," ");
             x = Integer.parseInt(st.nextToken());
             y = Integer.parseInt(st.nextToken());
-            z = Integer.parseInt(st.nextToken());
-            if(x==0 && y == 0&& z==0) break;
-            if(x>=y && y>=z){
-                bw.write(x*x == y*y + z*z?"right\n":"wrong\n");
-            } else if(y>=z && z>=x){
-                bw.write(y*y == x*x + z*z?"right\n":"wrong\n");
-            } else{
-                bw.write(z*z == x*x + y*y?"right\n":"wrong\n");
-            }
+            arr[i+1] = arr[i]-x+y;
         }
+        br.readLine();
+        bw.write(Arrays.stream(arr).max().getAsInt()+"");
         br.close();
         bw.flush();
         bw.close();
