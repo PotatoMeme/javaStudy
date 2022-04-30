bc7fb31fbab78f89d2e07de80897120d9d6cb5da
PotatoMeme <kshk0897@gmail.com>
2022-04-30 12:50:18 +0900

    20220430 백준 자바 10872 풀이

--- baekjoon/src/com/potatomeme/Baekjoon.java
+++ baekjoon/src/com/potatomeme/Baekjoon.java
@@ -3841,4 +3841,43 @@ public class Baekjoon {
         bw.close();
     }
 
+    //10872
+    {/*팩토리얼
+
+    문제
+    0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+
+    출력
+    첫째 줄에 N!을 출력한다.
+
+    예제 입력 1
+    10
+    예제 출력 1
+    3628800
+    예제 입력 2
+    0
+    예제 출력 2
+    1
+    */
+    }
+
+    public void solution10872() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        bw.write(solution10872_sub(n)+"");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    public int solution10872_sub(int n){
+        if(n<2){
+            return 1;
+        }
+        return n*solution10872_sub(n-1);
+    }
+
 }
--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -10,12 +10,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int r = Integer.parseInt(br.readLine());
-        bw.write(String.format("%.6f\n",r*r*Math.PI));
-        bw.write(String.format("%.6f\n",r*r*2.0)+"");
+        int n = Integer.parseInt(br.readLine());
+        bw.write(solution10872_sub(n)+"");
         br.close();
         bw.flush();
         bw.close();
+    }
+    public static int solution10872_sub(int n){
+        if(n<2){
+            return 1;
+        }
+        return n*solution10872_sub(n-1);
     }
 }
 
