4510c7737dbbe2be3d09621306957e5db9769661
PotatoMeme <kshk0897@gmail.com>
2022-04-30 12:43:28 +0900

    20220430 백준 자바 3053 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -10,13 +10,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < 5; i++) {
-            cnt += Integer.parseInt(st.nextToken()) == n ? 1 : 0;
-        }
-        bw.write(cnt + "");
+        int r = Integer.parseInt(br.readLine());
+        bw.write(String.format("%.6f\n",r*r*Math.PI));
+        bw.write(String.format("%.6f\n",r*r*2.0)+"");
         br.close();
         bw.flush();
         bw.close();
