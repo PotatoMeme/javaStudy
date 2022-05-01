c7c3f796a1e7aea79eda57de171a09ae934e8118
PotatoMeme <kshk0897@gmail.com>
2022-05-01 18:30:32 +0900

    20220501 백준 자바2711 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -1,6 +1,9 @@
 package com.potatomeme;
+
 import java.math.BigInteger;
+
 import static java.lang.Math.sqrt;
+
 import java.io.*;
 import java.util.*;
 
@@ -8,21 +11,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] arr = new int[10];
-        for (int i = 0; i < 10; i++) arr[i] = Integer.parseInt(br.readLine());
-        bw.write((int) Arrays.stream(arr).average().getAsDouble() + "\n");
-        Arrays.sort(arr);
-        int save = 1;
-        int check = 0;
-        int num = arr[0];
-        for (int i = 1; i < 10; i++) {
-            save = arr[i - 1] == arr[i] ? save + 1 : 0;
-            if (save > check) {
-                check = save;
-                num = arr[i - 1];
-            }
+        int n = Integer.parseInt(br.readLine());
+        int num;
+        StringTokenizer st;
+        String txt;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            num = Integer.parseInt(st.nextToken());
+            txt = st.nextToken();
+            bw.write(txt.substring(0, num - 1) + txt.substring(num) + "\n");
         }
-        bw.write(num + "");
         br.close();
         bw.flush();
         bw.close();
