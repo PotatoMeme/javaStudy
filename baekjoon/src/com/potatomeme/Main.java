67d09ad6f354d16cbf8dbd2c3ecf7d6c63c6005c
PotatoMeme <kshk0897@gmail.com>
2022-05-01 18:05:54 +0900

    20220501 백준 자바2592 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -1,25 +1,28 @@
 package com.potatomeme;
-
-import java.io.*;
 import java.math.BigInteger;
-import java.util.*;
-
 import static java.lang.Math.sqrt;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[9];
-        st.nextToken();
-        arr[0] = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < 8; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i + 1] = arr[i] - Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) arr[i] = Integer.parseInt(br.readLine());
+        bw.write((int) Arrays.stream(arr).average().getAsDouble() + "\n");
+        Arrays.sort(arr);
+        int save = 1;
+        int check = 0;
+        int num = arr[0];
+        for (int i = 1; i < 10; i++) {
+            save = arr[i - 1] == arr[i] ? save + 1 : 0;
+            if (save > check) {
+                check = save;
+                num = arr[i - 1];
+            }
         }
-        br.readLine();
-        bw.write(Arrays.stream(arr).max().getAsInt() + " ");
+        bw.write(num + "");
         br.close();
         bw.flush();
         bw.close();
