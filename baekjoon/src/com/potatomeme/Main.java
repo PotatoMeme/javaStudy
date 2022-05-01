f5c44714c3b3768b4a474ddd1997046c1f1b0f7f
PotatoMeme <kshk0897@gmail.com>
2022-05-01 19:39:35 +0900

    20220501 백준 자바10807 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -12,20 +12,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int t = Integer.parseInt(br.readLine());
-        String[] num;
-        for (int i = 0; i < t; i++) {
-            num = new StringBuffer(Integer.toBinaryString(Integer.parseInt(br.readLine())))
-                    .reverse()
-                    .toString()
-                    .split("");
-
-            for (int j = 0; j < num.length; j++) {
-                if (num[j].equals("1")) {
-                    bw.write(j + " ");
-                }
-            }
-            bw.write("\n");
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(br.readLine());
+        int check = 0;
+        for (int i = 0; i < t; i++) check = Integer.parseInt(st.nextToken()) == v ? check + 1 : check;
+        bw.write(check+"");
         br.close();
         bw.flush();
         bw.close();
