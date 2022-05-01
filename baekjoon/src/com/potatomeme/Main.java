59036977a00d6e5bc6aaa3c2f41fcedcc8fd0ede
PotatoMeme <kshk0897@gmail.com>
2022-05-01 19:13:07 +0900

    20220501 백준 자바1292 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -11,18 +11,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int sum, save, index;
-        save = index = 0;
-        StringTokenizer st;
-        for (int i = 1; i < 6; i++) {
-            st = new StringTokenizer(br.readLine());
-            sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-            if (save < sum) {
-                save = sum;
-                index = i;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        int num = 1;
+        int count = 0;
+        for (int i = 1; i <= end; i++) {
+            if (i >= start) {
+                sum += num;
+            }
+            count++;
+            if (num == count) {
+                num++;
+                count = 0;
             }
         }
-        bw.write(index+" "+save);
+        bw.write(sum + "");
         br.close();
         bw.flush();
         bw.close();
