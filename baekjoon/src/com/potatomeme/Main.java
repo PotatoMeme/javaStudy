44134d94247d32cac51a3d01a29e50025c2369ef
PotatoMeme <kshk0897@gmail.com>
2022-05-02 22:19:51 +0900

    20220502 백준 자바2558 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -1,30 +1,16 @@
 package com.potatomeme;
 
 import java.math.BigInteger;
-
 import static java.lang.Math.sqrt;
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int t = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int n;
-        int[] arr;
-        for (int i = 0; i < t; i++){
-            n = Integer.parseInt(br.readLine());
-            arr = new int[n];
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++){
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
-            int result = (Arrays.stream(arr).max().getAsInt()-Arrays.stream(arr).min().getAsInt())*2;
-            bw.write(result+"\n");
-        }
+        int sum = Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine());
+        bw.write(sum+" ");
         br.close();
         bw.flush();
         bw.close();
