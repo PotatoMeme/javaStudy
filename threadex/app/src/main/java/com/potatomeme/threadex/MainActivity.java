31d32ca1937805f1f782f75ce842f17c52f13e2c
PotatoMeme <kshk0897@gmail.com>
2022-05-06 00:03:32 +0900

    20220506 안드로이드 스레드,핸들러 예제

---
+++ threadex/app/src/main/java/com/potatomeme/threadex/MainActivity.java
@@ -0,0 +1,61 @@
+package com.potatomeme.threadex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView timeTv;
+    Button startBtn;
+
+    Runnable timeRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeTv = (TextView) findViewById(R.id.timeTv);
+        startBtn = (Button) findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(v->{
+            // Runnable 객체를 매개변수로 Thread 생성
+            Thread timeThread = new Thread(timeRunnable);
+            timeThread.start();  // Thread Start
+        });
+
+        timeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                int time = 0;
+                while(true){
+                    Message msg = new Message();
+                    msg.what = 0;
+                    msg.arg1 = time;
+                    handler.sendMessage(msg);    // Handler에 Message 보냄
+
+                    try{
+                        Thread.sleep(1000);
+                        time++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 0){
+                timeTv.setText(String.valueOf(msg.arg1));
+            }
+        }
+    };
+}
