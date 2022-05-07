524043091b57188c7edc2b91c6e593621fe45e7b
PotatoMeme <kshk0897@gmail.com>
2022-05-07 20:15:05 +0900

    20220507 백준자바 2752풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -11,13 +11,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-        int[] arr = new int[T];
-        for(int i = 0;i<T;i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i : Arrays.stream(arr).sorted().toArray()){
-            bw.write(i+"\n");
+        for (int i : Arrays.stream(arr).sorted().toArray()) {
+            bw.write(i + " ");
         }
         br.close();
         bw.flush();
