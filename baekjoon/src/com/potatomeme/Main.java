6c4d4b1c5d1cbfd0811d0d48400f7246436e9ccd
PotatoMeme <kshk0897@gmail.com>
2022-05-07 20:04:57 +0900

    20220507 백준자바 2750풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -8,50 +8,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] arr = new int[8];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int sum = 0;
-        int[] result = new int[5];
-        for (int i = 0; i < 8; i++) {
+        int T = Integer.parseInt(br.readLine());
+        int[] arr = new int[T];
+        for(int i = 0;i<T;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < 5; i++) {
-            result[i] = solution2822_sub();
-            sum += arr[result[i]];
-            arr[result[i]] = 0;
-        }
-        bw.write(sum + "\n");
-        int temp = 0;
-        for (int i = 1; i < 5; i++) {
-            for (int j = 0; j < 5 - i; j++) {
-                if (result[j] > result[j + 1]) {
-                    temp = result[j];
-                    result[j] = result[j + 1];
-                    result[j + 1] = temp;
-                }
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            bw.write(result[i]+1 + " ");
+        for(int i : Arrays.stream(arr).sorted().toArray()){
+            bw.write(i+"\n");
         }
         br.close();
         bw.flush();
         bw.close();
-    }
-
-    public static int solution2822_sub() {
-        int index = 0;
-        int compare = 0;
-        for (int i = 0; i < 8; i++) {
-            if (arr[i] > compare) {
-                compare = arr[i];
-                index = i;
-            }
-        }
-        return index;
     }
 }
 
