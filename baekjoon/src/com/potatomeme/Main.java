ff3b439001d2e7c0dde97d30af80622bace41ba5
PotatoMeme <kshk0897@gmail.com>
2022-05-08 10:28:00 +0900

    20220508 백준 자바 2587 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -11,17 +11,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int sum = 0;
-        int[] arr1 = new int[3];
-        int[] arr2 = new int[2];
-        for (int i = 0; i < 3; i++) {
-            arr1[i] = Integer.parseInt(br.readLine());
+        int[] arr = new int[5];
+        for(int i =0;i<5;i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < 2; i++) {
-            arr2[i] = Integer.parseInt(br.readLine());
-        }
-        sum = Arrays.stream(arr1).min().getAsInt() + Arrays.stream(arr2).min().getAsInt() - 50;
-        bw.write(sum + "");
+        bw.write(Arrays.stream(arr).sum()/5+"\n");
+        bw.write(Arrays.stream(arr).sorted().toArray()[2]+"");
         br.close();
         bw.flush();
         bw.close();
