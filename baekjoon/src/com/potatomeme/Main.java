033aca57c1b10f113829503c84f22b8a338e30e4
PotatoMeme <kshk0897@gmail.com>
2022-05-08 10:00:37 +0900

    20220508 백준 자바 1037 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -11,13 +11,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[3];
-        for (int i = 0; i < 3; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        for (int i : Arrays.stream(arr).sorted().toArray()) {
-            bw.write(i + " ");
+        if (num == 1) {
+            int result = Integer.parseInt(st.nextToken());
+            bw.write(result * result + "");
+        } else {
+            int[] arr = new int[num];
+            for (int i = 0; i < num; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            int result = Arrays.stream(arr).min().getAsInt() * Arrays.stream(arr).max().getAsInt();
+            bw.write(result + "");
         }
         br.close();
         bw.flush();
