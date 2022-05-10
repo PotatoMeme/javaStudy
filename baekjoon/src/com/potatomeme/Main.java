04b9f007f3798fcef3426e806b868d35f3d8ffdc
PotatoMeme <kshk0897@gmail.com>
2022-05-10 10:51:16 +0900

    20220510 백준 자바 9076 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -12,26 +12,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] arr = new int[9];
-        int sum = 0;
-        int saveA = 0, saveB = 0;
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-        }
-        Arrays.sort(arr);
-        for (int a = 0; a < 8; a++) {
-            for (int b = a + 1; b < 9; b++) {
-                if (sum - arr[a] - arr[b] == 100) {
-                    saveA = a;
-                    saveB = b;
-                    break;
-                }
+        int[] arr;
+        StringTokenizer st;
+        int sum;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            sum = 0;
+            arr = new int[5];
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < 5; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+                sum += arr[j];
             }
-        }
-        for (int j = 0; j < arr.length; j++) {
-            if (j == saveA || j == saveB) continue;
-            bw.write(arr[j] + "\n");
+            Arrays.sort(arr);
+            bw.write((arr[3] - arr[1]) >= 4 ? "KIN\n" : sum - arr[0] - arr[4] + "\n");
         }
         br.close();
         bw.flush();
