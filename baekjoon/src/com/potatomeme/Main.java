ea86be4ab61c3e130c74e8f54a54c59204a5283b
PotatoMeme <kshk0897@gmail.com>
2022-05-14 17:53:52 +0900

    20220514 백준 자바 2743 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -12,27 +12,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-        int total = Integer.parseInt(st.nextToken());
-        int[] arr = new int[T];
-        for (int i = 0; i < T; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        T--;
-        int cnt = 0;
-        int saveData;
-        while (true) {
-            if (total/arr[T] >= 1) {
-                saveData = total/arr[T];
-                total -= arr[T]*saveData;
-                cnt += saveData;
-            } else {
-                T--;
-            }
-            if (total == 0) break;
-        }
-        bw.write(cnt + "");
+        String word = br.readLine();
+        bw.write(word.length()+"");
+
         br.close();
         bw.flush();
         bw.close();
