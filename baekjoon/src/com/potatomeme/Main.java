1d8b703e90d351a4e3457fe1015da7b409e851f5
PotatoMeme <kshk0897@gmail.com>
2022-05-14 19:16:50 +0900

    20220514 백준 자바 1357 풀이

--- baekjoon/src/com/potatomeme/Main.java
+++ baekjoon/src/com/potatomeme/Main.java
@@ -12,14 +12,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-        int T = st.countTokens();
-        for (int i = 0; i < T; i++) {
-            bw.write(st.nextToken().charAt(0) + "");
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        int result = solution1357_sub(solution1357_sub(num1) + solution1357_sub(num2));
+        bw.write(result + "");
 
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    public static int solution1357_sub(int num) {
+        if (num >= 1000) {
+            return num % 10 * 1000 + num / 10 % 10 * 100 + num / 100 % 10 * 10 + +num / 1000;
+        } else if (num >= 100) {
+            return num % 10 * 100 + num / 10 % 10 * 10 + num / 100;
+        } else if (num >= 10) {
+            return num % 10 * 10 + num / 10;
+        } else {
+            return num;
+        }
     }
 }
