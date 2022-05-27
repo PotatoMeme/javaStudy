853614e3edc1e7136c788a01d8bf9f55ff15cc79
PotatoMeme <kshk0897@gmail.com>
2022-05-27 13:55:43 +0900

    20220527 프로그래머스 자바 12954 풀이

---
+++ baekjoon/src/com/potatomeme/may/week_4th/p_solution_12954.java
@@ -0,0 +1,11 @@
+package com.potatomeme.may.week_4th;
+
+public class p_solution_12954 {
+    public long[] solution(int x, int n) {
+        long[] answer = {};
+        for(int i = 0;i<n;i++){
+            answer[i] = x*(i+1);
+        }
+        return answer;
+    }
+}
