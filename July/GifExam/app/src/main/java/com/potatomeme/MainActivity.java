338b989ef6fea644a81bc112479c596e086ffeb7
PotatoMeme <kshk0897@gmail.com>
2022-07-02 18:20:29 +0900

    GifExam

---
+++ July/GifExam/app/src/main/java/com/potatomeme/MainActivity.java
@@ -0,0 +1,30 @@
+package com.potatomeme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.img_1);
+        Glide.with(this).load(R.raw.imoji).into(imageView);
+        button = findViewById(R.id.btn_next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+            }
+        });
+    }
+}
---
+++ July/GifExam/app/src/main/java/com/potatomeme/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.potatomeme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity2 extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        imageView = findViewById(R.id.img_1);
+        Glide.with(this).load(R.raw.imoji_ver33).into(imageView);
+    }
+}
