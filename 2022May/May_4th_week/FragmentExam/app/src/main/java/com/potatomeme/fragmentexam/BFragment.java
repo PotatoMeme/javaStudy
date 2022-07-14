a526bf14913b344a3509ba822db147add5918007
PotatoMeme <kshk0897@gmail.com>
2022-07-15 00:02:36 +0900

    20220715 안드로이드 study

--- 2022May/May_4th_week/FragmentExam/app/src/main/java/com/potatomeme/fragmentexam/BFragment.java
+++ 2022May/May_4th_week/FragmentExam/app/src/main/java/com/potatomeme/fragmentexam/BFragment.java
@@ -2,7 +2,6 @@ package com.potatomeme.fragmentexam;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
---
+++ July/3rd_week/PracticeSpace/app/src/main/java/com/potatomeme/MainActivity.java
@@ -0,0 +1,23 @@
+package com.potatomeme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.bnt_basicAct).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
